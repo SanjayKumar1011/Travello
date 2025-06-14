4937c6d08b22403b886eb141555eb6b40701f806
Sanjaykumar <rampellysanjaykumar454@gmail.com>
2025-06-14 17:32:49 +0530

    render.yaml indentation changes 2

--- travelloMain/settings.py
+++ travelloMain/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'travelloMain.wsgi.application'
 if os.environ.get('RENDER') == 'true':
     # Render deployment: use DATABASE_URL
     DATABASES = {
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'),conn_max_age=600)
     }
 else:
     # Local development
